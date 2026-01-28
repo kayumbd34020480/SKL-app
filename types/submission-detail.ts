02d166d72f8aff44135743a0d95e27354a5e4ac4
MD Kayum Islam <kayumislam1001@gmail.com>
2026-01-29 00:19:45 +0600

    Add files via upload

---
+++ types/submission-detail.ts
@@ -0,0 +1,11 @@
+export interface SubmissionDetail {
+  id: string;
+  taskTitle: string;
+  platform?: string;
+  description: string;
+  amount?: number;
+  status: "pending" | "approved" | "rejected";
+  type: "task" | "manual";
+  createdAt: Date;
+  reward?: number;
+}
---
+++ types/submission.ts
@@ -0,0 +1,4 @@
+export interface Submission {
+  taskId: string;
+  status: "pending" | "approved" | "rejected";
+}
